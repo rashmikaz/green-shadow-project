8505d17885f2c6da3e9f2b2dafee61673dc480c5
rashmikaz <phrashmika@gmail.com>
2024-11-15 02:27:52 +0530

    AppUtil.java

--- src/main/java/com/example/backend_spring_boot_final_project/util/AppUtil.java
+++ src/main/java/com/example/backend_spring_boot_final_project/util/AppUtil.java
@@ -1,10 +1,16 @@
 package com.example.backend_spring_boot_final_project.util;
 
 import java.util.Base64;
+import java.util.UUID;
 
 public class AppUtil {
     public static String cropImageToBase64(byte [] cropImage){
         return Base64.getEncoder().encodeToString(cropImage);
     }
+
+    public static String generateCropId(){
+        return "CROP-" + UUID.randomUUID();
+    }
+
 
 }
