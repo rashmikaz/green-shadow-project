5174b5a495cb0e16bff093a9c88b120ef3897b22
rashmikaz <phrashmika@gmail.com>
2024-12-06 14:52:48 +0530

    sign in in AuthServiceImpl

---
+++ src/main/java/com/example/backend_spring_boot_final_project/service/impl/AuthServiceImpl.java
@@ -0,0 +1,27 @@
+package com.example.backend_spring_boot_final_project.service.impl;
+
+import com.example.backend_spring_boot_final_project.dao.UserDao;
+import com.example.backend_spring_boot_final_project.service.AuthService;
+import com.example.backend_spring_boot_final_project.service.JWTService;
+import com.example.backend_spring_boot_final_project.util.Mapping;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthServiceImpl implements AuthService {
+    private final UserDao userDao;
+    private final Mapping mapping;
+    private final JWTService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    @Override
+    public JWTAuthResponse signIn(SignIn signIn) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(signIn.getEmail(), signIn.getPassword()));
+        var user = userDao.findByEmail(signIn.getEmail())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        var generatedToken = jwtService.generateToken(user);
+        System.out.println(generatedToken);
+        return JWTAuthResponse.builder().token(generatedToken).build();
+    }
+}
