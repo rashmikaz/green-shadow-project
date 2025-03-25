e24fa4b986e727ac413980ae9a50427b703cc7de
rashmikaz <phrashmika@gmail.com>
2025-03-26 00:00:50 +0530

    user controller

--- src/main/java/com/example/backend_spring_boot_final_project/controller/UserController.java
+++ src/main/java/com/example/backend_spring_boot_final_project/controller/UserController.java
@@ -1,6 +1,6 @@
-package com.example.backend_spring_boot_final_project.controller;
+package lk.ijse.green_shadow.controller;
 
-import com.example.backend_spring_boot_final_project.dto.impl.EquipmentDTO;
+
 import com.example.backend_spring_boot_final_project.dto.impl.UserDTO;
 import com.example.backend_spring_boot_final_project.exception.DataPersistException;
 import com.example.backend_spring_boot_final_project.service.UserService;
@@ -10,23 +10,24 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
 @RequestMapping("api/v1/user")
 @CrossOrigin
 public class UserController {
-
     @Autowired
     private UserService userService;
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> saveUser(@RequestBody UserDTO userDTO){
-
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveUser(@RequestBody UserDTO userDTO) {
         try {
             userService.saveUser(userDTO);
             return new ResponseEntity<>(HttpStatus.CREATED);
         }catch (DataPersistException e){
+            e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }catch (Exception e){
+            e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
