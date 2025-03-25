f103c06f1029a540f8cdf1c69e6739faef29c061
rashmikaz <phrashmika@gmail.com>
2025-03-25 23:52:56 +0530

    crop

--- src/main/java/com/example/backend_spring_boot_final_project/controller/CropController.java
+++ src/main/java/com/example/backend_spring_boot_final_project/controller/CropController.java
@@ -1,5 +1,6 @@
 package com.example.backend_spring_boot_final_project.controller;
 
+
 import com.example.backend_spring_boot_final_project.dto.CropStatus;
 import com.example.backend_spring_boot_final_project.dto.impl.CropDTO;
 import com.example.backend_spring_boot_final_project.dto.impl.FieldDTO;
@@ -15,10 +16,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-
 
 import java.util.List;
 import java.util.Optional;
@@ -28,40 +28,30 @@ import java.util.Optional;
 @RequestMapping("api/v1/crop")
 @CrossOrigin
 public class CropController {
-
-
     @Autowired
     private CropService cropService;
-
     @Autowired
     private FieldService fieldService;
 
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> saveCrop(@RequestParam("common_name") String commonName,
-                                         @RequestParam("scientific_name") String scientificName,
-                                         @RequestParam("crop_image") MultipartFile cropImage,
-                                         @RequestParam("category") String category,
-                                         @RequestParam("season") String season,
+    public ResponseEntity<Void> saveCrop(@RequestParam ("common_name") String commonName,
+                                         @RequestParam ("scientific_name") String scientificName,
+                                         @RequestPart ("crop_image") MultipartFile cropImage,
+                                         @RequestParam ("category") String category,
+                                         @RequestParam ("season") String season,
                                          @RequestParam ("field_name") String field_name
     ){
-
-
         String base64CropImage = "";
 
         try {
-
             FieldDTO field = fieldService.getFieldByName(field_name);
-
-
             byte[] bytesCropImage = cropImage.getBytes();
             base64CropImage = AppUtil.cropImageToBase64(bytesCropImage);
-
 
             String crop_code = AppUtil.generateCropId();
 
             CropDTO buildCropDTO = new CropDTO();
-
             buildCropDTO.setCrop_code(crop_code);
             buildCropDTO.setCommon_name(commonName);
             buildCropDTO.setScientific_name(scientificName);
@@ -69,67 +59,58 @@ public class CropController {
             buildCropDTO.setCategory(category);
             buildCropDTO.setSeason(season);
             buildCropDTO.setField(field);
-
             cropService.saveCrop(buildCropDTO);
-
             return new ResponseEntity<>(HttpStatus.CREATED);
-
         } catch (DataPersistException e) {
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        } catch (Exception e) {
+        }catch (Exception e){
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
     }
 
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<CropDTO> getAllCrops() {
-        return cropService.getAllCrops();
-    }
-
-
-    @GetMapping(value = "/{crop_Code}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public CropStatus getSelectedCrop(@PathVariable("crop_Code") String crop_code) {
-
-        if (!Regex.cropCodeMatcher(crop_code)) {
-            return new SelectedErrorStatus(1, "Crop code is invalid");
+    @GetMapping(value = "/{cropCode}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public CropStatus getSelectedCrop(@PathVariable ("crop_code") String crop_code){
+        if(!Regex.cropCodeMatcher(crop_code)){
+            return new SelectedErrorStatus(1,"Crop code is invalid");
         }
-
         return cropService.getCrop(crop_code);
     }
 
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<CropDTO> getAllCrops(){
+        return cropService.getAllCrops();
+    }
 
     @DeleteMapping(value = "/{cropCode}")
-    public ResponseEntity<Void> deleteCrop(@PathVariable("cropCode") String crop_code) {
+    public ResponseEntity<Void> deleteCrop(@PathVariable ("cropCode") String cropCode){
         try {
-            if (!Regex.cropCodeMatcher(crop_code)) {
+            if(!Regex.cropCodeMatcher(cropCode)){
                 return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
             }
-            cropService.deleteCrop(crop_code);
+            cropService.deleteCrop(cropCode);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (CropNotFoundException e) {
+        }catch (CropNotFoundException e){
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        } catch (Exception e) {
+        }catch (Exception e){
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-    }
 
+    }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PatchMapping(value = "/{commonName}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<CropEntity> updateCrop(@PathVariable @RequestParam("common_name") String commonName,
-                                                 @RequestParam("scientific_name") String scientificName,
-                                                 @RequestPart("crop_image") MultipartFile cropImage,
-                                                 @RequestParam("category") String category,
-                                                 @RequestParam("season") String season,
-                                                 @RequestParam("field_name") String field_name
-
-    ) {
+    public ResponseEntity<Void> updateCrop(@PathVariable @RequestParam ("common_name") String commonName,
+                                           @RequestParam ("scientific_name") String scientificName,
+                                           @RequestPart ("crop_image") MultipartFile cropImage,
+                                           @RequestParam ("category") String category,
+                                           @RequestParam ("season") String season,
+                                           @RequestParam ("field_name") String field_name
+    ){
         String base64CropImage = "";
 
         try {
@@ -139,21 +120,20 @@ public class CropController {
 
             String crop_code = AppUtil.generateCropId();
 
-            CropDTO cropDTO = new CropDTO();
-            cropDTO.setCrop_code(crop_code);
-            cropDTO.setCommon_name(commonName);
-            cropDTO.setScientific_name(scientificName);
-            cropDTO.setCrop_image(base64CropImage);
-            cropDTO.setCategory(category);
-            cropDTO.setSeason(season);
-            cropDTO.setField(field);
-
-            cropService.updateCrop(commonName, cropDTO);
+            CropDTO buildCropDTO = new CropDTO();
+            buildCropDTO.setCrop_code(crop_code);
+            buildCropDTO.setCommon_name(commonName);
+            buildCropDTO.setScientific_name(scientificName);
+            buildCropDTO.setCrop_image(base64CropImage);
+            buildCropDTO.setCategory(category);
+            buildCropDTO.setSeason(season);
+            buildCropDTO.setField(field);
+            cropService.updateCrop(commonName,buildCropDTO);
             return new ResponseEntity<>(HttpStatus.CREATED);
         } catch (DataPersistException e) {
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        } catch (Exception e) {
+        }catch (Exception e){
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
@@ -165,6 +145,7 @@ public class CropController {
         List<String> cropNames = cropService.getAllCropNames();
         return ResponseEntity.ok(cropNames);
     }
+
 
     @GetMapping("/getcropcode/{commonName}")
     public ResponseEntity<String> getCropCode(@PathVariable("commonName") String commonName){
@@ -179,4 +160,5 @@ public class CropController {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
 }
